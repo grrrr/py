80286e8e3c27fffa7dd48af7b6bc35b7e5fe7b6c
thomas <gr@grrrr.org>
2003-12-12 23:01:31 +0000

    small changes for release
    git-svn-id: https://svn.grrrr.org/ext/py/trunk@2179 4d9ac71a-51e6-0310-8455-cad1006bcd31

--- source/main.h
+++ source/main.h
@@ -25,7 +25,7 @@ WARRANTIES, see the file, "license.txt," in this distribution.
 #error You need at least flext version 0.4.5
 #endif
 
-#define PY__VERSION "0.1.2pre"
+#define PY__VERSION "0.1.2"
 
 
 #define PYEXT_MODULE "pyext" // name for module
