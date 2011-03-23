d0b8519d99441ebe29a1da74f27701a5f257f431
thomas <gr@grrrr.org>
2011-03-23 21:00:55 +0000

    some forgotten commits....
    git-svn-id: https://svn.grrrr.org/ext/trunk/py@3746 4d9ac71a-51e6-0310-8455-cad1006bcd31

--- source/pyprefix.h
+++ source/pyprefix.h
@@ -1,7 +1,7 @@
 /* 
 py/pyext - python script object for PD and MaxMSP
 
-Copyright (c)2002-2008 Thomas Grill (gr@grrrr.org)
+Copyright (c)2002-2009 Thomas Grill (gr@grrrr.org)
 For information on usage and redistribution, and for a DISCLAIMER OF ALL
 WARRANTIES, see the file, "license.txt," in this distribution.  
 
@@ -18,7 +18,7 @@ $LastChangedBy$
 
 // hack: must include math.h before Python.h (at least on OSX)
 // otherwise some functions don't get defined
-#include <math.h>
+#include <cmath>
 
 #if FLEXT_OS == FLEXT_OS_MAC
 #include <Python/Python.h>
